58c950a890a5468d3297c4552e1bb2b3f1447c80
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-09 17:58:27 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,12 @@
 #include<time.h>
 /* more headers go everywhere */
 /* betty style doc for function main goes there */
-int main(void)
+int main (void) /* comapre a number and return a string */
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/* My code starts here*/
 	if (n > 0)
 	{
