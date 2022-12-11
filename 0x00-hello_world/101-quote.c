540ba66c6e854ef9d03e2b64b42a27ab47f40cd8
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-11 15:51:29 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+* main -C program that prints a predefined string
+*
+* Return: return (1) when successful
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
