4ca589e8235f1f983e2fc0b1f1395e64b445a7bb
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-11 05:13:39 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - To print a string character using the function puts
+ *
+ * Retun: 0 is a success
+*/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
