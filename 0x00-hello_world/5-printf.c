e6545b0271ef8818475617095b51056604e5cf62
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-11 07:52:30 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main -A program to output a string using the print function
+*
+* Return: return 0 omce succeful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
