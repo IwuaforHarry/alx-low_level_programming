778348f8448fc4e2c72957ddfff7584d99252c66
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-12 17:40:48 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -A program to print all single base 10 numbers
+ *
+ * Return: return 0 once successful
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d\n", a);
+	return (0);
+}
