a6e816fd2eb3c0cfc806543c4b7bd6ea9f2d23e9
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-13 15:51:27 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - To print ten times the alphabets
+*/
+
+void print_alphabet_x10(void)
+{
+	char Harold;
+
+	int 0zi;
+
+	Ozi = 0;
+	while (Ozi < 10)
+	{
+		for (Harold = 'a'; Harold <= 'z'; Harold++)
+			_putchar(Harold);
+	}
+	_putchar('\n');
+	Ozi++;
+}
