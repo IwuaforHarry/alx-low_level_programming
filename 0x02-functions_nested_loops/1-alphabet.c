52a29d817bbc53b38e44afbeca0b1ec89f08bd2e
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-13 15:03:54 -0800

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char Harold;
+
+	for (Harold = 'a'; Harold <= 'z'; Harold++)
+		_putchar(Harold);
+	_putchar('\n');
+	return (0);
+}
