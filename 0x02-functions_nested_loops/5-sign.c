5e543736d14f3247896baf0166e0224e895c3a1f
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-14 15:20:46 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main -program that prints different signs
+ *
+ * Return: return 1, 0 or -1 signs accordingly
+*/
+
+int main(void)
+{
+	int n;
+
+	int count;
+
+	n = count;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		for (n < 0)
+	{
+		_putchar('-');
+		return ('-'1);
+	}
+}
