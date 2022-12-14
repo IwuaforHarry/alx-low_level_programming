370270e2b89aa385427ac29b968149ad285ea958
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-13 17:19:07 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_islower - program to print lower case letters
+ *
+ * Return: return 1 when successful otherwise return 0
+*/
+
+int _islower(void)
+{
+	int Harry;
+
+	if (Harry >= 'a' && Harry <= 'z')
+		return (1);
+	else
+		return (0);
+}
