f398f12dda615ac9d5552b0d6d76915555e7e8c9
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-14 18:12:37 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - a function to add numbers
+ *
+ * @a: a is a given integer
+ * @b: b is another integer variable
+ * Return: Return the absolute value
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
