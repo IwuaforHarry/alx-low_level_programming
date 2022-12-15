fc93117713d4f29f99e99bb03623efa78c3a3248
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-14 16:59:20 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer -to print every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int hour;
+
+	int minutes;
+
+	hour = 0;
+	while (hour < 24)
+	{
+		minutes = 0;
+		while (minutes < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+		minutes++
+	}
+	hour++
+}
