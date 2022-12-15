d50adf13c70be4379a3c7b83222d58cfc2f60f20
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-14 16:03:40 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - program to compute an absolute value
+ * @t: Value to be printed
+ * Return: return exact value of t
+*/
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
