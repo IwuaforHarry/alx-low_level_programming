e85df652e436c6588c02b74c5ed5e1771665e5cd
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-19 13:01:38 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - function to print a string to standard output
+ * @str: The string variable to print
+ * Returns: returns lenght of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != \0)
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
