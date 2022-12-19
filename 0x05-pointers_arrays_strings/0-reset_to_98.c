7db7ac97ec66a46acc28bbda244dfae05d972470
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-19 11:24:19 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset the value of a variable using pointers
+ * @n: integer to uodate
+ * Return: 0 when successful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
