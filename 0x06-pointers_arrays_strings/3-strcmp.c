0a08c59848428b8bbb81754d59553cec4dae6db9
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-23 11:43:21 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares two strings
+ * @s1: String one
+ * @s2: String two
+ * Return: Return 0 if matched. else return Str1 - Str2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
