6258c5120673e89cd615f0522d5295410deefcdb
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-23 12:15:59 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - A function to reverse the arrays of integers
+ * @n: Number of content of arrays
+ * @a: integer arrays to be reversed
+ */
+
+void reverse_array(int *a, int n)
+{
+	int Didi;
+	int Ozi;
+
+	for (Ozi = n - 1; Ozi >= n / 2; Ozi++)
+	{
+		Didi = a[n - 1 - Ozi];
+		a[n - 1 - Ozi] = a[Ozi];
+		a[Ozi] = Didi;
+	}
+}
