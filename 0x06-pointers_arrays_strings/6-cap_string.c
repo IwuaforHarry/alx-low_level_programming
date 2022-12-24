baf5a0c8b01b3ec3eeae294bfb6e8f2e35f37058
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2022-12-23 17:02:50 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,11 @@ char *cap_string(char *str);
 {
 	int i = 0;
 
-	while (str[++i])
+	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
+
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
@@ -26,8 +27,11 @@ char *cap_string(char *str);
 				str[i - 1] == '(' ||
 				str[i - 1] == ')' ||
 				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||)
+				str[i - 1] == '}' ||
+				i == 0)
 			str[i] -= 32;
+
+		i++;
 	}
 	return (str);
 }
