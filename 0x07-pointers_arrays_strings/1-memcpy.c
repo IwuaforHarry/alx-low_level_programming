dff9de3f756c78517840d8baacaa915e947b1357
IwuaforHarry <118133731+IwuaforHarry@users.noreply.github.com>
2023-01-02 16:09:23 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int dad;
 
-	for (dad = 0; dad < n, dad++)
+	for (dad = 0; dad < n; dad++)
 		*(dest + dad) = *(src + dad);
 
 	return (dest);
